86b5017ed75355f6e51847ac6380d46258fbac9b
bgzitos <lucaslopes49441008@gmail.com>
2026-04-19 16:47:45 -0300

    Criação da Classe C

---
+++ classes/src/main/java/C.java
@@ -0,0 +1,13 @@
+public class C {
+
+    private String C1;
+    private Integer C2;
+
+    public static void MC1() {
+        System.out.println("MC1");
+    }
+
+    public static void MC2() {
+        System.out.println("MC2");
+    }
+}
