a7f3c29ec3fc78c539c09815cfeab7e41b4aca8d
bgzitos <lucaslopes49441008@gmail.com>
2026-04-19 17:12:58 -0300

    Criada a Classe D com atributos e métodos

--- classes/src/main/java/D.java
+++ classes/src/main/java/D.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class D {
+
+    private Character D1;
+    private Double D2;
+
+    public void MD1() {
+        System.out.println("MD1");
+    }
+
+    public void MD2() {
+        System.out.println("MD2");
+    }
 }
