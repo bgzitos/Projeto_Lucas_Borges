ef7ce1d8293f509e8475860c704c9562fa14ae04
bgzitos <lucaslopes49441008@gmail.com>
2026-04-19 16:17:02 -0300

    Classes A e B com atributos e métodos

---
+++ classes/src/main/java/A.java
@@ -0,0 +1,29 @@
+public class A {
+
+    private Integer A1;
+    private Float A2;
+
+    public Integer getA1() {
+        return A1;
+    }
+
+    public void setA1(Integer a1) {
+        A1 = a1;
+    }
+
+    public Float getA2() {
+        return A2;
+    }
+
+    public void setA2(Float a2) {
+        A2 = a2;
+    }
+
+    public static void MA1() {
+        System.out.println("MA1");
+    }
+
+    public static void MA2() {
+        System.out.println("MA2");
+    }
+}
---
+++ classes/src/main/java/B.java
@@ -0,0 +1,29 @@
+public class B {
+
+    private Integer B1;
+    private Float B2;
+
+    public Integer getB1() {
+        return B1;
+    }
+
+    public void setB1(Integer b1) {
+        B1 = b1;
+    }
+
+    public Float getB2() {
+        return B2;
+    }
+
+    public void setB2(Float b2) {
+        B2 = b2;
+    }
+
+    public static void MB1() {
+        System.out.println("MB1");
+    }
+
+    public static void MB2() {
+        System.out.println("MB2");
+    }
+}
